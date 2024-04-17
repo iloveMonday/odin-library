8492a42d1b23f6b0acb7b74b8f34c7ad2c177734
IloveMonday <iloveyouchandlerbing@gmail.com>
2024-04-17 14:50:14 -0400

    fix Remove button

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let readButton = document.querySelectorAll(".read-button");
-
 let addBookButton = document.getElementById("add-book");
 let bookModal = document.getElementById("book-modal");
 let close = document.querySelector(".close");
@@ -8,13 +6,11 @@ let bookContainer = document.querySelector(".books");
 
 const myLibrary = [];
 
-function Book(title, author, pages, read, index, removeIndex) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.index = index;
-  this.removeIndex = removeIndex;
 //   this.info = function () {
 //     return title + " by " + author + ", " + pages + " pages, " + read;
 //   };
@@ -67,9 +63,7 @@ function addBookToLibrary() {
   let newTitle = document.querySelector(".title").value;
   let newAuthor = document.querySelector(".author").value;
   let newPages = document.querySelector(".pages").value;
-//   let newRead = document.querySelector(".read").value;
-
-    let newRead = "";
+  let newRead = "";
     let rd = document.getElementById("read");
     let unrd = document.getElementById("unread");
     if (rd.checked==true){
@@ -78,10 +72,7 @@ function addBookToLibrary() {
     else if (unrd.checked==true){
         newRead = "Unread";
     }
-
-  let newIndex = (myLibrary.length);
-  let newRemoveIndex = ("rm" + myLibrary.length);
-  let newBook = new Book(newTitle, newAuthor, newPages, newRead, newIndex, newRemoveIndex);
+  let newBook = new Book(newTitle, newAuthor, newPages, newRead);
   myLibrary.push(newBook);
   event.preventDefault();
 }
@@ -99,43 +90,78 @@ function clear() {
 
 function displayBooks(){ 
     bookContainer.innerHTML = "";
-    myLibrary.forEach((e) =>{
-            let newCard = document.createElement("div");
-            newCard.className = "book-card";
-            let title = document.createElement("h2");
-            let author = document.createElement("p");
-            let pages = document.createElement("p");
-            let read = document.createElement("button")
-            let remove = document.createElement("button");
-            title.innerHTML = e.title;
-            author.innerHTML = "By "+e.author;
-            pages.innerHTML = e.pages+" pages";
+//     myLibrary.forEach((e) =>{
+//             let newCard = document.createElement("div");
+//             newCard.className = "book-card";
+//             let title = document.createElement("h2");
+//             let author = document.createElement("p");
+//             let pages = document.createElement("p");
+//             let read = document.createElement("button")
+//             let remove = document.createElement("button");
+//             title.innerHTML = e.title;
+//             author.innerHTML = "By "+e.author;
+//             pages.innerHTML = e.pages+" pages";
 
-            if (e.read == "Read"){
-                read.style.backgroundColor = "green";
-                read.innerHTML = "Read";
-            }
-            else if (e.read == "Unread") {
-                read.style.backgroundColor = "red";
-                read.innerHTML = "unread";
-            }
+//             if (e.read == "Read"){
+//                 read.style.backgroundColor = "green";
+//                 read.innerHTML = "Read";
+//             }
+//             else if (e.read == "Unread") {
+//                 read.style.backgroundColor = "red";
+//                 read.innerHTML = "unread";
+//             }
 
-            // read.className = "read-button"+(e.index);
-            read.innerHTML = e.read;
+//             // read.className = "read-button"+(e.index);
+//             read.innerHTML = e.read;
+//             read.className = "read-button";
+//             remove.className = "remove-button";
+//             remove.innerHTML = "Remove";
+//             newCard.setAttribute("data-attribute", e.index);
+//             newCard.appendChild(title);
+//             newCard.appendChild(author);
+//             newCard.appendChild(pages);
+//             newCard.appendChild(read);
+//             newCard.appendChild(remove);
+//             bookContainer.appendChild(newCard);  
+// });       
 
-            remove.className = "remove-button";
-            remove.setAttribute("id", "rm" + e.index);
-            remove.innerHTML = "Remove";
-            newCard.setAttribute("id", e.index);
-            newCard.appendChild(title);
-            newCard.appendChild(author);
-            newCard.appendChild(pages);
-            newCard.appendChild(read);
-            newCard.appendChild(remove);
-            bookContainer.appendChild(newCard);  
-            
-});       
+for (let i = 0; i < myLibrary.length; i++){
+  let newCard = document.createElement("div");
+  newCard.className = "book-card";
+  let title = document.createElement("h2");
+  let author = document.createElement("p");
+  let pages = document.createElement("p");
+  let read = document.createElement("button")
+  read.className = "read-button";
+  read.setAttribute("data-index", i);
+  let remove = document.createElement("button");
+  remove.className = "remove-button";
+  remove.innerHTML = "Remove";
+  remove.setAttribute("data-index", i);
+
+  title.innerHTML = myLibrary[i].title; 
+  author.innerHTML = "By "+myLibrary[i].author;
+  pages.innerHTML = myLibrary[i].pages+" pages";
+
+  if (myLibrary[i].read == "Read"){
+      read.style.backgroundColor = "green";
+      read.innerHTML = "Read";
+  }
+  else if (myLibrary[i].read == "Unread") {
+      read.style.backgroundColor = "red";
+      read.innerHTML = "Unread";
+  }
+
+  newCard.appendChild(title);
+  newCard.appendChild(author);
+  newCard.appendChild(pages);
+  newCard.appendChild(read);
+  newCard.appendChild(remove);
+  bookContainer.appendChild(newCard);  
+}
+
     removeBook();
+    toggleRead();
     console.log(myLibrary);
 }
 
@@ -148,7 +174,24 @@ function removeBook(){
       displayBooks();
     }));
 }
-    //console.log(document.getElementById('email').value);
+
+function toggleRead(){
+  let readButton = document.querySelectorAll(".read-button");
+  readButton.forEach((e) => 
+    e.addEventListener("click", () =>{
+      let i = e.dataset.index;
+      // if (myLibrary[i].read == "Read"){
+      //   myLibrary[i].read == "Unread";
+      // } else if 
+      // (myLibrary[i].read == "Unread"){
+      //   myLibrary[i].read == "Read";
+      // }
+      console.log(myLibrary[i].index);
+
+    displayBooks();
+    }));
+}
+
 
 
 addBookButton.addEventListener("click", () => {
@@ -156,11 +199,3 @@ addBookButton.addEventListener("click", () => {
   clear();
   displayBooks();
 });
-
-
-
-// readButton.addEventListener("click", () =>{
-    
-// })
-
-// someArray.splice(x, 1);
